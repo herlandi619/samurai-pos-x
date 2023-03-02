2f08e78eb8e5abeb8abcf99d555ffaa0453630b7
renold herlandi <herlandi619@gmail.com>
2023-03-02 17:46:19 +0700

    revisi lgii

--- src/App.jsx
+++ src/App.jsx
@@ -18,27 +18,27 @@ const App = () => {
       
         <Navbar />
 
-       <div className=' relative -z-20 md:overflow-hidden'>
-       <img src={bg} className='absolute w-full h-full -z-10 grayscale opacity-40' alt="bg" />
+       <div className=''>
+   
 
-        <div className='p-24 mx-auto'>
+        <div className='p-24 mx-auto bg-slate-900'>
         
-          <h1 className='text-lg font-bold text-center bg-[#8FE4FF] rounded-lg max-w-sm mx-auto md:text-2xl'>Aplikasi kasir online</h1>
+          <h1 className='text-lg p-2 text-[#8FE4FF] font-bold text-center  rounded-lg max-w-sm mx-auto md:text-2xl'>Aplikasi kasir online</h1>
 
           <div className='max-w-md mx-auto mt-4 '>
-          <h3 className='text-3xl font-bold ] text-slate-700 text-center lg:max-w-xl mx-auto md:text-4xl '>berbasis cloud yang dapat membawa </h3>
+          <h3 className='text-3xl font-bold ] text-slate-100 text-center lg:max-w-xl mx-auto md:text-4xl '>berbasis cloud yang dapat membawa </h3>
           
-          <p className='text-3xl font-bold text-slate-700 text-center md:text-4xl'> potensi usaha baik</p>
+          <p className='text-3xl font-bold text-slate-100 text-center md:text-4xl'> potensi usaha baik</p>
           </div>
               
               <div className='flex justify-center'>
-              <Typed className='text-3xl font-bold text-slate-700 text-center' strings={['Online', 'Maupun', 'Offline']} typeSpeed={120} backSpeed={140} loop />
+              <Typed className='text-3xl font-bold text-slate-100 text-center' strings={['Online', 'Maupun', 'Offline']} typeSpeed={120} backSpeed={140} loop />
               </div>
          
 
 
           <span className='flex justify-center p-5'>
-          <button className='mx-auto py-3 px-16 bg-[#8FE4FF] text-slate-900 font-bold text-xl rounded-xl shadow-xl mt-3 hover:scale-105 duration-300 '>Get Started</button>
+          <button className='mx-auto py-3 px-16 bg-[#8FE4FF] text-slate-100 font-bold text-xl rounded-xl shadow-slate-100 mt-3 hover:scale-105 duration-300 '>Get Started</button>
           </span>
           <div className='py-12'></div>
         </div>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,14 +8,14 @@ const Hero = () => {
     <div className=' lg:flex xl:flex 2xl:flex'>
 
         <div className='p-20 '>
-            <h1 className='text-2xl font-semibold font-[Golos Text] text-slate-700 text-center md:text-3xl lg:text-4xl'>Atur Kasir Lebih Mudah</h1>
+            <h1 className='text-2xl font-semibold cover-full font-[Golos Text] text-slate-700 text-center md:text-3xl lg:text-4xl'>Atur Kasir Lebih Mudah</h1>
 
             <h1 className='text-4xl font-semibold font-[Golos Text] text-slate-700 text-center  md:text-5xl lg:text-5xl'>Dengan <span className='text-[#8FE4FF]'>X-SM</span> POS</h1>
 
             <h3 className='text-slate-700  text-center my-5 md:text-md lg:text-xl'>Kami Membantu Bisnis Anda Mempercepat Proses Penjualan Dengan Aplikasi Kasir MOKA</h3>
 
             <span className='flex justify-center'>
-            <button className='py-3 px-3 bg-black text-white font-[Golos Text] rounded-xl shadow-xl items-center my-2 hover:scale-105 duration-300 '>COBA GRATIS SEKARANG</button>
+            <button className='py-3 px-3 bg-[#8FE4FF] text-white font-[Golos Text] rounded-xl shadow-xl items-center my-2 hover:scale-105 duration-300 '>COBA GRATIS SEKARANG</button>
             </span>
 
                 <div className='flex justify-center'>
@@ -25,7 +25,7 @@ const Hero = () => {
 
             </div>
 
-            <div className='lg:my-auto'>
+            <div className='lg:my-auto pb-20'>
 
                 <img src={hero} alt="hero" className='w-1/2 mx-auto'/>
 
